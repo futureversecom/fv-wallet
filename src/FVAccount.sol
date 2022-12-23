5bdbb97ff29fbc37def0325c8534e423f4132635
zees-dev <dev.786zshan@gmail.com>
2022-12-23 15:30:32 +1300

    refactor FV account, add tests for FV account and registry

--- src/FVAccount.sol
+++ src/FVAccount.sol
@@ -1,55 +1,49 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.17;
 
+import "@openzeppelin/contracts/proxy/Clones.sol";
 import "@lukso/lsp-smart-contracts/contracts/LSP0ERC725Account/LSP0ERC725Account.sol";
 import "@lukso/lsp-smart-contracts/contracts/LSP6KeyManager/LSP6KeyManager.sol";
 
-contract FVAccountRegistry {
-  LSP0ERC725Account immutable public fvAccount;
-  LSP6KeyManager immutable public fvKeyManager;
+/**
+* @dev reverts when address `addr` is already registered
+* @param addr the address to register
+*/
+error AccountAlreadyExists(address addr);
 
-  mapping(address => address) public accounts;
+library Utils {
+  bytes16 private constant SYMBOLS = "0123456789abcdef";
+  uint8 private constant ADDRESS_LENGTH = 20;
 
-  event AccountRegistered(address indexed account);
-
-  constructor() {
-    fvAccount = new LSP0ERC725Account(address(this));
-    fvKeyManager = new LSP6KeyManager(address(fvAccount));
-  }
-
-  function identityOf(address addr) public view returns (address) {
-    return accounts[addr];
-  }
-
-  // TODO - move to library
-  bytes16 private constant _SYMBOLS = "0123456789abcdef";
-  uint8 private constant _ADDRESS_LENGTH = 20;
-  function toHexStringNoPrefix(uint256 value, uint256 length) public pure returns (string memory) {
+  function toHexStringNoPrefix(uint256 value, uint256 length) internal pure returns (string memory) {
     bytes memory buffer = new bytes(2 * length);
     for (uint256 i = 2 * length + 1; i > 1; --i) {
-      buffer[i - 2] = _SYMBOLS[value & 0xf];
+      buffer[i - 2] = SYMBOLS[value & 0xf];
       value >>= 4;
     }
     require(value == 0, "Strings: hex length insufficient");
     return string(buffer);
   }
-  function toHexStringNoPrefix(address addr) public pure returns (string memory) {
-    return toHexStringNoPrefix(uint256(uint160(addr)), _ADDRESS_LENGTH);
+
+  function toHexStringNoPrefix(address addr) internal pure returns (string memory) {
+    return toHexStringNoPrefix(uint256(uint160(addr)), ADDRESS_LENGTH);
   }
-  function stringToBytes32(string memory source) public pure returns (bytes32 result) {
+
+  function stringToBytes32(string memory source) internal pure returns (bytes32 result) {
     bytes memory tempEmptyStringTest = bytes(source);
     if (tempEmptyStringTest.length == 0) {
         return 0x0;
     }
-
     assembly {
         result := mload(add(source, 32))
     }
   }
-  function toBytes(bytes32 data) public pure returns (bytes memory) {
+
+  function toBytes(bytes32 data) internal pure returns (bytes memory) {
     return bytes.concat(data);
   }
-  function fromHexChar(uint8 c) public pure returns (uint8) {
+
+  function fromHexChar(uint8 c) internal pure returns (uint8) {
     if (bytes1(c) >= bytes1('0') && bytes1(c) <= bytes1('9')) {
       return c - uint8(bytes1('0'));
     }
@@ -61,8 +55,9 @@ contract FVAccountRegistry {
     }
     revert("fail");
   }
+
   // Convert an hexadecimal string to raw bytes
-  function fromHex(string memory s) public pure returns (bytes memory) {
+  function toBytes(string memory s) public pure returns (bytes memory) {
     bytes memory ss = bytes(s);
     require(ss.length%2 == 0); // length must be even
     bytes memory r = new bytes(ss.length/2);
@@ -72,28 +67,90 @@ contract FVAccountRegistry {
     return r;
   }
 
-  function register(address addr) public {
-    if (accounts[addr] != address(0)) return;
+  function permissionsKey(address _addr) public pure returns (bytes32) {
+    return bytes32(toBytes(string.concat("4b80742de2bf82acb3630000", toHexStringNoPrefix(_addr))));
+  }
+}
+
+contract FVAccountRegistry {
+  using Utils for address;
+  using Utils for string;
+
+  // All Permissions currently exclude REENTRANCY, DELEGATECALL and SUPER_DELEGATECALL for security
+  // source: https://github.com/lukso-network/lsp-smart-contracts/blob/b97b186430eb4e4984c6c366356d62119d5930cc/constants.js#L182
+  string public constant ALL_PERMISSIONS = "00000000000000000000000000000000000000000000000000000000003f3f7f";
+  string public constant NO_PERMISSION = "0000000000000000000000000000000000000000000000000000000000000000";
+
+  LSP0ERC725Account immutable public fvAccount;
+  LSP6KeyManager immutable public fvKeyManager;
+
+  mapping(address => address) public accounts;
+
+  /**
+  * @notice Emitted when registering a new address
+  * @param account The address of the account registered
+  */
+  event AccountRegistered(address indexed account);
+
+  constructor() {
+    fvAccount = new LSP0ERC725Account(address(this));
+    fvKeyManager = new LSP6KeyManager(address(fvAccount));
+    
+    // add permission to set the owner of the account
+    fvAccount.setData(
+      address(this).permissionsKey(),
+      ALL_PERMISSIONS.toBytes()
+    );
+
+    fvAccount.transferOwnership(address(fvKeyManager));
+    fvKeyManager.execute(abi.encode(fvAccount.acceptOwnership.selector));
+
+    // remove permission required to set the owner of the account from this contract
+    fvKeyManager.execute(
+      abi.encodeWithSelector(
+        bytes4(keccak256("setData(bytes32,bytes)")),
+        address(this).permissionsKey(),
+        NO_PERMISSION.toBytes()
+      )
+    );
+  }
+
+  function identityOf(address _addr) public view returns (address) {
+    return accounts[_addr];
+  }
+
+  function register(address _addr) public returns (address) {
+    if (accounts[_addr] != address(0)) revert AccountAlreadyExists(_addr);
+
+    // https://docs.openzeppelin.com/contracts/4.x/api/proxy#Clones
+    // address addrKeyManager = Clones.clone(address(fvAccount));
+    // address addrKeyManager = Clones.cloneDeterministic(address(fvAccount), keccak256(abi.encodePacked(_addr)));    
+
     LSP0ERC725Account acc = new LSP0ERC725Account(address(this));
     LSP6KeyManager kmgr = new LSP6KeyManager(address(acc));
 
     // temporarily give SUPER permissions to self
-    acc.setData(
-      bytes32(fromHex(string.concat("4b80742de2bf82acb3630000", toHexStringNoPrefix(address(this))))),
-      fromHex("00000000000000000000000000000000000000000000000000000000003f3f7f")
-    );
+    acc.setData(address(this).permissionsKey(), ALL_PERMISSIONS.toBytes());
+
     // give SUPER permissions to user
-    acc.setData(
-      bytes32(fromHex(string.concat("4b80742de2bf82acb3630000", toHexStringNoPrefix(addr)))),
-      fromHex("00000000000000000000000000000000000000000000000000000000003f3f7f")
-    );
+    acc.setData(_addr.permissionsKey(), ALL_PERMISSIONS.toBytes());
+
     acc.transferOwnership(address(kmgr));
     kmgr.execute(abi.encode(acc.acceptOwnership.selector));
+
     // remove SUPER permissions from self
-    kmgr.execute(abi.encodeWithSelector(bytes4(keccak256("setData(bytes32,bytes)")),
-      bytes32(fromHex(string.concat("4b80742de2bf82acb3630000", toHexStringNoPrefix(address(this))))),
-      fromHex("0000000000000000000000000000000000000000000000000000000000000000")
-    ));
-    accounts[addr] = address(kmgr);
+    kmgr.execute(
+      abi.encodeWithSelector(
+        bytes4(keccak256("setData(bytes32,bytes)")),
+        address(this).permissionsKey(),
+        NO_PERMISSION.toBytes()
+      )
+    );
+
+    accounts[_addr] = address(kmgr);
+
+    emit AccountRegistered(_addr);
+
+    return address(kmgr);
   }
 }
---
+++ test/FVAccount.t.sol
@@ -0,0 +1,87 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+import "forge-std/Test.sol";
+import "@lukso/lsp-smart-contracts/contracts/LSP6KeyManager/LSP6KeyManager.sol";
+import {FVAccountRegistry, AccountAlreadyExists, Utils} from "../src/FVAccount.sol";
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+contract MockERC20 is ERC20 {
+  constructor() ERC20("MyToken", "MTK") {}
+  function mint(address to, uint256 amount) public {
+    _mint(to, amount);
+  }
+}
+
+contract FVAccountRegistryTest is Test {
+  FVAccountRegistry public fvAccountRegistry;
+  MockERC20 public mockERC20;
+
+  // re-declare event for assertions
+  event AccountRegistered(address indexed account);
+
+  function setUp() public {
+    fvAccountRegistry = new FVAccountRegistry();
+    mockERC20 = new MockERC20();
+  }
+
+  function testFVAccountRegistryIsNotFVAccountOwner() public {
+    assertFalse(fvAccountRegistry.fvAccount().owner() == address(fvAccountRegistry));
+  }
+
+  function testFVAccountRegistryHasNoPermissions() public {
+    bytes memory registryPermissions = fvAccountRegistry.fvAccount().getData(Utils.permissionsKey(address(fvAccountRegistry)));
+    assertEq(registryPermissions, Utils.toBytes(fvAccountRegistry.NO_PERMISSION()));
+  }
+
+  function testFVAccountOwnerIsKeyManager() public {
+    assertEq(fvAccountRegistry.fvAccount().owner(), address(fvAccountRegistry.fvKeyManager()));
+  }
+
+  function testRegisterOfZeroAddress() public {
+    vm.expectEmit(true, false, false, false, address(fvAccountRegistry));
+
+    // We emit the event we expect to see.
+    emit AccountRegistered(address(0));
+
+    // Perform the actual call (which should emit expected event).
+    fvAccountRegistry.register(address(0));
+  }
+
+  function testRegisterOfNewAddressSucceeds() public {
+    vm.expectEmit(true, false, false, false, address(fvAccountRegistry));
+
+    emit AccountRegistered(address(this));
+
+    address userKeyManagerAddr = fvAccountRegistry.register(address(this));
+    assertTrue(userKeyManagerAddr != address(0));
+
+    LSP6KeyManager userKeyManager = LSP6KeyManager(userKeyManagerAddr);
+    assertTrue(userKeyManager.target() != address(0));
+  }
+
+  function testRegisterFailsForMultipleRegistrations() public {
+    fvAccountRegistry.register(address(this));
+    vm.expectRevert(abi.encodeWithSelector(AccountAlreadyExists.selector, address(this)));
+    fvAccountRegistry.register(address(this));
+  }
+
+  function testRegisteredUserCanCallExternalContract() public {
+    address userKeyManagerAddr = fvAccountRegistry.register(address(this));
+
+    // abi encoded call to mint 100 tokens to address(this)
+    bytes memory mintCall = abi.encodeWithSelector(mockERC20.mint.selector, address(this), 100);
+
+    // abi encoded call to execute mint call
+    bytes memory executeCall = abi.encodeWithSignature(
+      "execute(uint256,address,uint256,bytes)", // operationType, target, value, data
+      0,
+      address(mockERC20),
+      0,
+      mintCall
+    );
+    LSP6KeyManager(userKeyManagerAddr).execute(executeCall);
+
+    assertEq(mockERC20.balanceOf(address(this)), 100);
+  }
+}
