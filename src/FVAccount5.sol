9dc153b8ac985dbffa7738276ef90202e61b59d9
Michael Standen <michael.standen@alteredstatemachine.xyz>
2022-12-30 10:17:34 +1300

    Base v5 on v4

--- src/FVAccount5.sol
+++ src/FVAccount5.sol
@@ -1,35 +1,38 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.17;
 
-import "@openzeppelin/contracts/proxy/beacon/BeaconProxy.sol";
-import "@openzeppelin/contracts/proxy/beacon/UpgradeableBeacon.sol";
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import {ERC165} from "@openzeppelin/contracts/utils/introspection/ERC165.sol";
+import {BeaconProxy} from "@openzeppelin/contracts/proxy/beacon/BeaconProxy.sol";
+import {UpgradeableBeacon} from "@openzeppelin/contracts/proxy/beacon/UpgradeableBeacon.sol";
 
-import "@lukso/lsp-smart-contracts/contracts/LSP0ERC725Account/LSP0ERC725Account.sol";
-import "@lukso/lsp-smart-contracts/contracts/LSP6KeyManager/LSP6KeyManager.sol";
+import {LSP0ERC725Account} from "@lukso/lsp-smart-contracts/contracts/LSP0ERC725Account/LSP0ERC725Account.sol";
+import {LSP6KeyManager} from "@lukso/lsp-smart-contracts/contracts/LSP6KeyManager/LSP6KeyManager.sol";
+import {LSP0ERC725AccountInit} from "@lukso/lsp-smart-contracts/contracts/LSP0ERC725Account/LSP0ERC725AccountInit.sol";
+import {LSP6KeyManagerInit} from "@lukso/lsp-smart-contracts/contracts/LSP6KeyManager/LSP6KeyManagerInit.sol";
 
-import "@lukso/lsp-smart-contracts/contracts/LSP6KeyManager/LSP6KeyManagerInit.sol";
-
-import "./LSP0ERC725AccountLateInit.sol";
-import "./IFVAccountRegistry.sol";
+import {IFVAccountRegistry} from "./IFVAccountRegistry.sol";
 import "./Utils.sol";
+import "./LSP0ERC725AccountLateInit.sol";
 
-// v3 base
+// v4 base
 
-contract FVAccountRegistry is IFVAccountRegistry {
+contract FVAccountRegistry is Initializable, OwnableUpgradeable, ERC165, IFVAccountRegistry {
   using Utils for string;
 
-  // TODO: make FVAccountRegistry contract upgradable (Transparent vs UUPS proxy)
-  // TODO: remember to inherit from Initializable and correct proxies
-  // TODO: should have initializer instead of constructor - initialize every base contract (including ownable)
-  // TODO: use initializer modifiers
-  // TODO: https://docs.openzeppelin.com/upgrades-plugins/1.x/writing-upgradeable
-
-  UpgradeableBeacon immutable public fvAccountBeacon;
-  UpgradeableBeacon immutable public fvKeyManagerBeacon;
-
+  UpgradeableBeacon public fvAccountBeacon;
+  UpgradeableBeacon public fvKeyManagerBeacon;
   mapping(address => address) public accounts;
 
   constructor() {
+    _disableInitializers();
+  }
+
+  function initialize() external virtual initializer {
+    // init initializers
+    __Ownable_init();
+
     // Deploy initializable ERC725Account (LSP0) and LSP6KeyManager contracts
     LSP0ERC725AccountLateInit fvAccount = new LSP0ERC725AccountLateInit();
     LSP6KeyManagerInit fvKeyManager = new LSP6KeyManagerInit();
@@ -39,8 +42,6 @@ contract FVAccountRegistry is IFVAccountRegistry {
     fvKeyManagerBeacon = new UpgradeableBeacon(address(fvKeyManager));
   }
 
-  // TODO: Note gas costs for previous tests
-  // TODO: benchmark/compare gas costs
   function register(address _addr) public returns (address) {
     if (accounts[_addr] != address(0)) revert AccountAlreadyExists(_addr);
 
@@ -54,7 +55,6 @@ contract FVAccountRegistry is IFVAccountRegistry {
 
     address userFVKeyManagerAddr = address(userFVKeyManagerProxy); // gas savings (repeated access)
 
-    // Initialise key manager as owner and SUPER permissions to user
     LSP0ERC725AccountLateInit(payable(address(userFVAccountProxy))).initialize(
       userFVKeyManagerAddr,
       Utils.permissionsKey(KEY_ADDRESSPERMISSIONS_PERMISSIONS, _addr),
