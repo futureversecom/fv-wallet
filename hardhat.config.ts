deb31b9607bdefc030cb4588bb66d12b23c40fe1
zees-dev <dev.786zshan@gmail.com>
2023-01-13 23:04:51 +1300

    local and porcini deployment setup

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,59 @@
 import { HardhatUserConfig } from "hardhat/config";
+import "hardhat-preprocessor";
 import "@nomicfoundation/hardhat-toolbox";
 
+import fs from "fs";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+function getRemappings() {
+  return fs
+    .readFileSync("remappings.txt", "utf8")
+    .split("\n")
+    .filter(Boolean) // remove empty lines
+    .map((line: string) => line.trim().split("="));
+}
+
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+  solidity: {
+    version: "0.8.17",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  preprocess: {
+    eachLine: (hre) => ({
+      transform: (line: string) => {
+        if (line.match(/^\s*import /i)) {
+          for (const [from, to] of getRemappings()) {
+            if (line.includes(from)) {
+              line = line.replace(from, to);
+              break;
+            }
+          }
+        }
+        return line;
+      },
+    }),
+  },
+  paths: {
+    sources: "./src",
+    cache: "./cache_hardhat",
+  },
+  networks: {
+    localhost: {
+      url: "http://localhost:8545",
+      accounts: [process.env.PRIVATE_KEY!],
+    },
+    porcini: {
+      url: "https://porcini.au.rootnet.app/",
+      accounts: [ process.env.PRIVATE_KEY! ],
+    },
+  },
 };
 
 export default config;
