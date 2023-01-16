61829a3f54a00717164f760338925870a3c11214
zees-dev <dev.786zshan@gmail.com>
2023-01-17 01:01:55 +1300

    updated deployment script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -4,17 +4,17 @@ import dotenv from "dotenv";
 dotenv.config();
 
 async function main() {
-  // deploy LSP0ERC725AccountLateInit contract
-  const fvAccountFactory = await ethers.getContractFactory("LSP0ERC725AccountLateInit");
+  // deploy FVIdentity contract
+  const fvAccountFactory = await ethers.getContractFactory("FVIdentity");
   const fvAccount = await fvAccountFactory.deploy();
   await fvAccount.deployed();
-  console.log("LSP0ERC725AccountLateInit deployed to:", fvAccount.address);
+  console.log("FVIdentity deployed to:", fvAccount.address);
 
-  // deploy LSP6KeyManagerInit contract
-  const lsp6KeyManagerInitFactory = await ethers.getContractFactory("LSP6KeyManagerInit");
-  const lsp6KeyManagerInit = await lsp6KeyManagerInitFactory.deploy();
-  await lsp6KeyManagerInit.deployed();
-  console.log("LSP6KeyManagerInit deployed to:", lsp6KeyManagerInit.address);
+  // deploy FVKeyManager contract
+  const keyManagerFactory = await ethers.getContractFactory("FVKeyManager");
+  const fvKeyManager = await keyManagerFactory.deploy();
+  await fvKeyManager.deployed();
+  console.log("FVKeyManager deployed to:", fvKeyManager.address);
 
   // deploy Utils library
   const utilsLibFactory = await ethers.getContractFactory("Utils");
@@ -22,23 +22,23 @@ async function main() {
   await utilsLib.deployed();
   console.log("[library] Utils deployed to:", utilsLib.address);
 
-  // deploy FVAccountRegistry contract - with linked Utils library
-  const fvAccountRegistryFactory = await ethers.getContractFactory("FVAccountRegistry", {
+  // deploy FVIdentityRegistry contract - with linked Utils library
+  const fvIdentityRegistryFactory = await ethers.getContractFactory("FVIdentityRegistry", {
     libraries: { Utils: utilsLib.address, },
   });
-  const fvAccountRegistry = await fvAccountRegistryFactory.deploy();
-  await fvAccountRegistry.deployed();
-  console.log("FVAccountRegistry deployed to:", fvAccountRegistry.address);
+  const fvIdentityRegistry = await fvIdentityRegistryFactory.deploy();
+  await fvIdentityRegistry.deployed();
+  console.log("FVIdentityRegistry deployed to:", fvIdentityRegistry.address);
 
   // deploy TransparentUpgradeableProxy contract with PUBLIC_ADDRESS as admin
-  // initialize FVAccountRegistry contract
+  // initialize FVIdentityRegistry contract
   const transparentUpgradeableProxyFactory = await ethers.getContractFactory("TransparentUpgradeableProxy");
   const transparentUpgradeableProxy = await transparentUpgradeableProxyFactory.deploy(
-    fvAccountRegistry.address,
+    fvIdentityRegistry.address,
     process.env.PUBLIC_ADDRESS!,
     // abi.encodeWithSignature("initialize(address,address)", fvAccountImpl, keyManagerImpl)
     (new ethers.utils.Interface(["function initialize(address,address)"]))
-      .encodeFunctionData("initialize", [fvAccount.address, lsp6KeyManagerInit.address])
+      .encodeFunctionData("initialize", [fvAccount.address, fvKeyManager.address])
   );
   await transparentUpgradeableProxy.deployed();
   console.log("TransparentUpgradeableProxy deployed to:", transparentUpgradeableProxy.address);
